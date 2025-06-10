dad4a5a994a1bb4128d7b8ec58536e68d05c65f5
FarukPie <farukatak.official@gmail.com>
2025-06-10 20:07:57 +0300

    I have added some components

---
+++ StockFlow/ViewComponents/_NavbarDashboardComponentPartial.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StockFlow.ViewComponents
+{
+    public class _NavbarDashboardComponentPartial: ViewComponent
+    {
+      public IViewComponentResult Invoke()
+      {
+          return View();
+        }
+
+
+
+    }
+}
