40db635429ceb48da321050ab179008bfb985e4d
FarukPie <farukatak.official@gmail.com>
2025-06-11 13:12:22 +0300

    Completed all components and database processes

---
+++ StockFlow/ViewComponents/_SidebarDashboardComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StockFlow.ViewComponents
+{
+    public class _SidebarDashboardComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+    
+    
+}
