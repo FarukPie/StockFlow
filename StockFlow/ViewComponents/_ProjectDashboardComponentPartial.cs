24bd5e9350b4db75d7ea486ec3ec8a1c1acf4ae3
FarukPie <farukatak.official@gmail.com>
2025-06-14 13:21:24 +0300

    Completed dashboard section

---
+++ StockFlow/ViewComponents/_ProjectDashboardComponentPartial.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockFlow.Context;
+
+namespace StockFlow.ViewComponents
+{
+    public class _ProjectDashboardComponentPartial : ViewComponent
+
+    {
+        private readonly StoreContext _context;
+
+        public _ProjectDashboardComponentPartial(StoreContext context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+           
+            return View();
+        }
+
+    }
+}
