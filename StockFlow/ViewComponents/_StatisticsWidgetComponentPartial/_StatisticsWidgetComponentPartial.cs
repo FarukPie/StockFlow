a69bee16417581fc91c6d9b669a9adf635c397d2
FarukPie <farukatak.official@gmail.com>
2025-06-15 11:37:26 +0300

    completed highest and lowest price products

--- StockFlow/ViewComponents/_StatisticsWidgetComponentPartial/_StatisticsWidgetComponentPartial.cs
+++ StockFlow/ViewComponents/_StatisticsWidgetComponentPartial/_StatisticsWidgetComponentPartial.cs
@@ -1,11 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StockFlow.Context;
 
 namespace StockFlow.ViewComponents._StatisticsViewComponent
 {
     public class _StatisticsWidgetComponentPartial : ViewComponent
     {
+        private readonly StoreContext _context;
+
+        public _StatisticsWidgetComponentPartial(StoreContext context)
+        {
+            _context = context;
+        }
+
         public IViewComponentResult Invoke()
         {
+            ViewBag.CategoryCount= _context.Categories.Count();
+            ViewBag.ProductMaxPrice = _context.Products.Max(x => x.ProductPrice);
+            ViewBag.ProductMinPrice = _context.Products.Min(x => x.ProductPrice);
+            ViewBag.ProductMaxPriceProduct=_context.Products.Where(x=>x.ProductPrice==(_context.Products.Max(x=>x.ProductPrice))).Select(x=>x.ProductName).FirstOrDefault();
+            ViewBag.ProductMinPriceProduct=_context.Products.Where(x=>x.ProductPrice==(_context.Products.Min(x=>x.ProductPrice))).Select(x=>x.ProductName).FirstOrDefault();
             return View();
         }
     }
