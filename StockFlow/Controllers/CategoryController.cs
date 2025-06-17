2bdd68d89365fd74765b18514148cdd40a44a03a
FarukPie <farukatak.official@gmail.com>
2025-06-17 15:31:07 +0300

    completed all categories processes

--- StockFlow/Controllers/CategoryController.cs
+++ StockFlow/Controllers/CategoryController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using StockFlow.Context;
+using StockFlow.Entities;
 
 namespace StockFlow.Controllers
 {
@@ -13,8 +14,44 @@ namespace StockFlow.Controllers
 
         public IActionResult CategoryList()
         {
-            var value=_context.Categories.ToList();
+            var value = _context.Categories.ToList();
             return View(value);
+        }
+
+        [HttpGet]
+        public IActionResult AddCategory()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddCategory(Category category)
+        {
+            category.CategoryStatus = false;
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return RedirectToAction("CategoryList");
+        }
+        public IActionResult DeleteCategory(int id)
+        {
+            var value = _context.Categories.Find(id);
+            _context.Categories.Remove(value);
+            _context.SaveChanges();
+            return RedirectToAction("CategoryList");
+        }
+        [HttpGet]
+        public IActionResult UpdateCategory(int id)
+        {
+            var value = _context.Categories.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult UpdateCategory(Category category)
+        {
+            _context.Categories.Update(category);
+            _context.SaveChanges();
+            return RedirectToAction("CategoryList");
+
         }
     }
 }
