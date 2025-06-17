85d5f31d2fc46710ad1dafb2cda1c64d5341e683
FarukPie <farukatak.official@gmail.com>
2025-06-17 14:42:22 +0300

    category section added

--- StockFlow/Controllers/CategoryController.cs
+++ StockFlow/Controllers/CategoryController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StockFlow.Context;
 
 namespace StockFlow.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly StoreContext _context;
+        public CategoryController(StoreContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult CategoryList()
         {
-            return View();
+            var value=_context.Categories.ToList();
+            return View(value);
         }
     }
 }
